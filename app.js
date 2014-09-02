802c02b3aafdd07916d395b3d4e00b51671cc18c
Austin Bedford <abedford.ab@gmail.com>
2014-09-02 13:15:02 -0700

    Image html added in js file
    removed redundant code and added ing src html to append.
    and new load button

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 $(document).ready(function () {
 
-  //var getImages = function(access_parameters) {}
-  function getImages(access_parameters) { 
-  }
-
-// the parameters we need to pass in our request to Instagrams's API
-  var result = $.ajax({
+    // the parameters we need to pass in our request to Instagrams's API
+    var result = $.ajax({
     url: "https://api.instagram.com/v1/tags/sunset/media/recent?client_id=878ba6d5363242fcb0b06b0ac1f883c6",
     dataType: "jsonp", 
     type: "GET",
@@ -13,16 +9,19 @@ $(document).ready(function () {
 
     //loop the image responses to view multiple images
     result.done(function(msg){
-      for (var i = 0; i < msg.data.length; i++) {
-        msg.data.length[i]
-      };
+    for (var i = 0; i < msg.data.length; i++) {
+        $('#target').append('<img src="' + msg.data[i].images.standard_resolution.url + '" />');
+    }
+      })
 
-      $("#target").html(msg.data[0].images.standard_resolution.url);
-      console.log(msg);
-    });
-  
-    $('#target').append('');
-  
+
+    //Append load more to the div
+    $("#more" ).click(function() {  
+    var next_url = $(this).attr('next_url');
+    loadEmUp(next_url);
+    return false;
+  });
+
   /*initialize oath library w.client ID
   OAuth.initialize('878ba6d5363242fcb0b06b0ac1f883c6');
 
