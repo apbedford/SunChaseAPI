32e5bc1f9ce921bc8df0146411247d5aaf9b6405
Austin Bedford <abedford.ab@gmail.com>
2014-10-17 11:39:48 -0700

    Update 10/17/2014
    CSS file renamed
    Content properties modified to smaller size
    photo size reduced by percentage

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 $(document).ready(function () {
-
+    
     // the parameters we need to pass in our request to Instagrams's API
     var result = $.ajax({
-    url: "https://api.instagram.com/v1/tags/sunset/media/recent?client_id=878ba6d5363242fcb0b06b0ac1f883c6",
-    dataType: "jsonp", 
-    type: "GET",
+      url: "https://api.instagram.com/v1/tags/sunset/media/recent?client_id=878ba6d5363242fcb0b06b0ac1f883c6",
+      dataType: "jsonp", 
+      type: "GET",
     });
 
     //loop the image responses to view multiple images
     result.done(function(msg){
-    for (var i = 0; i < msg.data.length; i++) {
+      for (var i = 0; i < msg.data.length; i++) {
         $('#target').append('<img src="' + msg.data[i].images.standard_resolution.url + '" />');
-    }
-      })
-
+        
+        if (msg.data[i].caption !==null) {
+          if(msg.data[i].caption.text !== null) {
+            var title = msg.data[i].caption.text;
+          }
+        } else {
+          var title = "";
+        }
+      }
+    })
 
     //Append load more to the div
     $("#more" ).click(function() {  
-    var next_url = $(this).attr('next_url');
-    loadEmUp(next_url);
-    return false;
-  });
+      var next_url = $(this).attr('next_url');
+      loadEmUp(next_url);
+      return false;
+    });
 
   /*initialize oath library w.client ID
   OAuth.initialize('878ba6d5363242fcb0b06b0ac1f883c6');
@@ -30,5 +37,5 @@ $(document).ready(function () {
     res.get('/v1/tags/sunset/media/recent').done(function(data) {
       console.log(data);
     });
-  });*/
+});*/
 });
